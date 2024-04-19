209c193a5f97996ea195e4f4d92b67c6d0ce36ad
Hyunseok Ko <kohyunsuk98@gmail.com>
2024-04-19 18:45:10 +0900

    Feature | #2 | @lcomment | api 모듈 초기 세팅

---
+++ cakk-api/src/main/java/com/cakk/api/Application.java
@@ -0,0 +1,20 @@
+package com.cakk.api;
+
+import java.util.TimeZone;
+import jakarta.annotation.PostConstruct;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	@PostConstruct
+	public static void started() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+	}
+
+	public static void main(String[] args) {
+		started();
+		SpringApplication.run(Application.class, args);
+	}
+}
---
+++ cakk-api/src/main/java/com/cakk/api/common/response/ApiResponse.java
@@ -0,0 +1,4 @@
+package com.cakk.api.common.response;
+
+public class ApiResponse {
+}
