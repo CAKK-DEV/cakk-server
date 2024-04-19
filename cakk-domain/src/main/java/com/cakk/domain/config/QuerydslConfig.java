5e2c3967afca43ee317378033c74518d0717399b
Hyunseok Ko <kohyunsuk98@gmail.com>
2024-04-19 18:45:04 +0900

    Feature | #2 | @lcomment | domain 모듈 초기 세팅

---
+++ cakk-domain/src/main/java/com/cakk/domain/config/QuerydslConfig.java
@@ -0,0 +1,20 @@
+package com.cakk.domain.config;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.querydsl.jpa.JPQLTemplates;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+@Configuration
+public class QuerydslConfig {
+
+	@PersistenceContext
+	private EntityManager entityManager;
+
+	@Bean
+	public JPAQueryFactory queryFactory() {
+		return new JPAQueryFactory(JPQLTemplates.DEFAULT, entityManager);
+	}
+}
