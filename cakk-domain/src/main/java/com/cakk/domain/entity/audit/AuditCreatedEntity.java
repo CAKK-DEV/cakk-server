e4dad80879bd45449d9a3c7e0916ee43cb137d78
lcomment <kohyunsuk98@gmail.com>
2024-05-01 11:04:19 +0900

    feature | #3 | @lcomment | Entity 생성

---
+++ cakk-domain/src/main/java/com/cakk/domain/entity/audit/AuditCreatedEntity.java
@@ -0,0 +1,21 @@
+package com.cakk.domain.entity.audit;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class AuditCreatedEntity {
+
+	@CreatedDate
+	@Column(updatable = false)
+	private LocalDateTime createdAt;
+}
---
+++ cakk-domain/src/main/java/com/cakk/domain/entity/user/BusinessInformation.java
@@ -0,0 +1,72 @@
+package com.cakk.domain.entity.user;
+
+import java.time.LocalTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.MapsId;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import com.cakk.domain.entity.audit.AuditEntity;
+import com.cakk.domain.entity.shop.CakeShop;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "business_information")
+public class BusinessInformation extends AuditEntity {
+
+	@Id
+	@Column(name = "shop_id")
+	private Long id;
+
+	@Column(name = "operation_day", nullable = false, length = 7)
+	private String operationDay;
+
+	@Column(name = "start_time", nullable = false)
+	private LocalTime startTime;
+
+	@Column(name = "end_time", nullable = false)
+	private LocalTime endTime;
+
+	@Column(name = "business_number", length = 20)
+	private String businessNumber;
+
+	@OneToOne
+	@MapsId
+	@JoinColumn(name = "shop_id")
+	private CakeShop cakeShop;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id", referencedColumnName = "user_id")
+	private User user;
+
+	@Builder
+	public BusinessInformation(
+		CakeShop cakeShop,
+		String operationDay,
+		LocalTime startTime,
+		LocalTime endTime,
+		String businessNumber,
+		User user
+	) {
+		this.id = cakeShop.getId();
+		this.cakeShop = cakeShop;
+		this.operationDay = operationDay;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.businessNumber = businessNumber;
+		this.user = user;
+	}
+
+}
