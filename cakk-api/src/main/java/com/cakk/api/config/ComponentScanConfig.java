a82a450f282dae99e1e03c1f4eb9e811cad71cb5
lcomment <kohyunsuk98@gmail.com>
2024-05-02 22:53:26 +0900

    Chore | #4 | @lcomment | api 모듈에 component scan 추가

---
+++ cakk-api/src/main/java/com/cakk/api/config/ComponentScanConfig.java
@@ -0,0 +1,12 @@
+package com.cakk.api.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = {
+	"com.cakk.client",
+	"com.cakk.domain"
+})
+public class ComponentScanConfig {
+}
