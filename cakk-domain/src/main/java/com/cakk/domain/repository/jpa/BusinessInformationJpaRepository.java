cbaf3166d65b58357091efe7a4140534fb3f85f6
김태용 <amico741@naver.com>
2024-05-08 20:03:07 +0900

    Fix | #5 | @YongsHub | 네이티브 쿼리에서 JPA 메서드 방식으로 변경

--- cakk-domain/src/main/java/com/cakk/domain/repository/jpa/BusinessInformationJpaRepository.java
+++ cakk-domain/src/main/java/com/cakk/domain/repository/jpa/BusinessInformationJpaRepository.java
@@ -3,12 +3,11 @@ package com.cakk.domain.repository.jpa;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 import com.cakk.domain.entity.user.BusinessInformation;
 
 public interface BusinessInformationJpaRepository extends JpaRepository<BusinessInformation, Long> {
 
-	@Query("select bi from BusinessInformation as bi join fetch bi.cakeShop where bi.cakeShop.id =:cakeShopId")
 	Optional<BusinessInformation> findBusinessInformationByCakeShopId(Long cakeShopId);
+
 }
