ab227d1a719649d2f2becd735604178178cefeea
김태용 <amico741@naver.com>
2024-05-09 14:00:12 +0900

    Feature | #14 | @YongsHub | 외부 연동 서비스와 비동기 설정 추가

---
+++ cakk-api/src/main/java/com/cakk/api/config/AsyncConfig.java
@@ -0,0 +1,9 @@
+package com.cakk.api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class AsyncConfig {
+}
--- cakk-api/src/main/java/com/cakk/api/event/shop/listener/CertificationEventListener.java
+++ cakk-api/src/main/java/com/cakk/api/event/shop/listener/CertificationEventListener.java
@@ -1,5 +1,6 @@
 package com.cakk.api.event.shop.listener;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -14,6 +15,7 @@ import com.cakk.domain.event.shop.CertificationEvent;
 public class CertificationEventListener {
 	private final SlackService slackService;
 
+	@Async
 	@TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
 	public void sendMessageToSlack(CertificationEvent certificationEvent) {
 	}
