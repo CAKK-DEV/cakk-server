b08834feb6ac8d465a9004550d3878b69396461d
Hyunseok Ko <kohyunsuk98@gmail.com>
2024-05-09 18:52:49 +0900

    Chore | #4 | @lcomment | TestContainer 및 테스트 환경 구축

---
+++ cakk-api/src/test/java/com/cakk/api/common/base/ServiceTest.java
@@ -0,0 +1,37 @@
+package com.cakk.api.common.base;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.navercorp.fixturemonkey.FixtureMonkey;
+import com.navercorp.fixturemonkey.api.introspector.BuilderArbitraryIntrospector;
+import com.navercorp.fixturemonkey.api.introspector.ConstructorPropertiesArbitraryIntrospector;
+import com.navercorp.fixturemonkey.api.introspector.FieldReflectionArbitraryIntrospector;
+
+import com.cakk.domain.config.JpaConfig;
+
+@Import(JpaConfig.class)
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
+public abstract class ServiceTest {
+
+	protected final FixtureMonkey getConstructorMonkey() {
+		return FixtureMonkey.builder()
+			.objectIntrospector(ConstructorPropertiesArbitraryIntrospector.INSTANCE)
+			.build();
+	}
+
+	protected final FixtureMonkey getReflectionMonkey() {
+		return FixtureMonkey.builder()
+			.objectIntrospector(FieldReflectionArbitraryIntrospector.INSTANCE)
+			.build();
+	}
+
+	protected final FixtureMonkey getBuilderMonkey() {
+		return FixtureMonkey.builder()
+			.objectIntrospector(BuilderArbitraryIntrospector.INSTANCE)
+			.build();
+	}
+}
