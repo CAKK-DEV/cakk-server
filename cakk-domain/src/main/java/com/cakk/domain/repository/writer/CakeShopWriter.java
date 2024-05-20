8dfd78a251e6c3260598cbe8901292b3116ea97c
김태용 <amico741@naver.com>
2024-05-20 13:15:04 +0900

    Refactor | #21 | @YongsHub | 영업일 ERD 추가로 인한 비즈니스 로직 리팩토링

--- cakk-domain/src/main/java/com/cakk/domain/repository/writer/CakeShopWriter.java
+++ cakk-domain/src/main/java/com/cakk/domain/repository/writer/CakeShopWriter.java
@@ -1,12 +1,16 @@
 package com.cakk.domain.repository.writer;
 
+import java.util.List;
+
 import lombok.RequiredArgsConstructor;
 
 import com.cakk.domain.annotation.Writer;
+import com.cakk.domain.entity.cake.CakeShopOperation;
 import com.cakk.domain.entity.shop.CakeShop;
 import com.cakk.domain.entity.user.BusinessInformation;
 import com.cakk.domain.repository.jpa.BusinessInformationJpaRepository;
 import com.cakk.domain.repository.jpa.CakeShopJpaRepository;
+import com.cakk.domain.repository.jpa.CakeShopOperationJpaRepository;
 
 @Writer
 @RequiredArgsConstructor
@@ -14,9 +18,11 @@ public class CakeShopWriter {
 
 	private final BusinessInformationJpaRepository businessInformationJpaRepository;
 	private final CakeShopJpaRepository cakeShopJpaRepository;
+	private final CakeShopOperationJpaRepository cakeShopOperationJpaRepository;
 
-	public void createCakeShop(CakeShop cakeShop, BusinessInformation businessInformation) {
+	public void createCakeShop(CakeShop cakeShop, List<CakeShopOperation> cakeShopOperations, BusinessInformation businessInformation) {
 		cakeShopJpaRepository.save(cakeShop);
+		cakeShopOperationJpaRepository.saveAll(cakeShopOperations);
 		businessInformationJpaRepository.save(businessInformation);
 	}
 }
