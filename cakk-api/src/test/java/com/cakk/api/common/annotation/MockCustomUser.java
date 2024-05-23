45c4ac9fddd231b4cba483a3f09b6c75f8defe9b
Hyunseok Ko <kohyunsuk98@gmail.com>
2024-05-23 11:16:43 +0900

    Refactor | #34 | @lcomment | 패키지 변경

--- cakk-api/src/test/java/com/cakk/api/common/annotation/MockCustomUser.java
+++ cakk-api/src/test/java/com/cakk/api/common/annotation/MockCustomUser.java
@@ -1,8 +1,9 @@
-package com.cakk.api.config;
+package com.cakk.api.common.annotation;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import org.springframework.security.test.context.support.WithSecurityContext;
+import com.cakk.api.common.config.WithMockCustomUserSecurityContextFactory;
 
 @Retention(RetentionPolicy.RUNTIME)
 @WithSecurityContext(factory = WithMockCustomUserSecurityContextFactory.class)
