90fa6a4965562eafe1fff4ed73dab7cb1dccb755
김태용 <amico741@naver.com>
2024-05-24 21:30:09 +0900

    Test | #42 | @YongsHub | 케이크 샵 관리자에 의한 생성 unit test 추가

--- cakk-api/src/test/java/com/cakk/api/service/shop/ShopServiceTest.java
+++ cakk-api/src/test/java/com/cakk/api/service/shop/ShopServiceTest.java
@@ -15,12 +15,15 @@ import net.jqwik.api.Arbitraries;
 
 import com.cakk.api.common.annotation.TestWithDisplayName;
 import com.cakk.api.common.base.ServiceTest;
+import com.cakk.api.dto.request.shop.CreateShopRequest;
 import com.cakk.api.dto.response.shop.CakeShopDetailResponse;
 import com.cakk.api.dto.response.shop.CakeShopSimpleResponse;
 import com.cakk.api.mapper.ShopMapper;
 import com.cakk.common.enums.ReturnCode;
 import com.cakk.common.exception.CakkException;
 import com.cakk.domain.dto.param.shop.CakeShopDetailParam;
+import com.cakk.domain.entity.shop.CakeShop;
+import com.cakk.domain.entity.user.BusinessInformation;
 import com.cakk.domain.dto.param.shop.CakeShopSimpleParam;
 import com.cakk.domain.repository.reader.CakeShopReader;
 import com.cakk.domain.repository.reader.UserReader;
@@ -43,6 +46,18 @@ public class ShopServiceTest extends ServiceTest {
 
 	@Mock
 	private ApplicationEventPublisher publisher;
+
+	private CreateShopRequest getCreateShopRequestFixture() {
+		return getConstructorMonkey().giveMeBuilder(CreateShopRequest.class)
+			.set("businessNumber", Arbitraries.strings().withCharRange('a', 'z').ofMinLength(1).ofMaxLength(7))
+			.set("shopName", Arbitraries.strings().withCharRange('a', 'z').ofMinLength(1).ofMaxLength(20))
+			.set("shopBio", Arbitraries.strings().withCharRange('a', 'z').ofMaxLength(20))
+			.set("shopDescription", Arbitraries.strings().withCharRange('a', 'z').ofMaxLength(20))
+			.set("startTimes", Arbitraries.of(LocalTime.now()).list().ofSize(3))
+			.set("endTimes", Arbitraries.of(LocalTime.now()).list().ofSize(3))
+			.set("operationsDays", Arbitraries.of(Days.class).list().ofSize(3))
+			.sample();
+	}
 
 	@TestWithDisplayName("id로 케이크 샵을 간단조회 한다.")
 	void searchSimpleById1() {
@@ -121,5 +136,19 @@ public class ShopServiceTest extends ServiceTest {
 			.hasMessageContaining(ReturnCode.NOT_EXIST_CAKE_SHOP.getMessage());
 
 		verify(cakeShopReader, times(1)).searchDetailById(cakeShopId);
+	}
+
+	@TestWithDisplayName("Admin에 의해 케이크 샵을 생성하는데 성공한다")
+	void createCakeShop() {
+		//given
+		CreateShopRequest request = getCreateShopRequestFixture();
+		System.out.println(request);
+
+		//when
+		shopService.createCakeShopByCertification(request);
+
+		//verify
+		verify(cakeShopWriter, times(1))
+			.createCakeShop(any(CakeShop.class), anyList(), any(BusinessInformation.class));
 	}
 }
