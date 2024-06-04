29b7e606de6c7207731a6c39746d59432fc634b3
Hyunseok Ko <kohyunsuk98@gmail.com>
2024-06-04 11:05:26 +0900

    Feature | #60 | @lcomment | 유저 탈퇴 비즈니스 구현

--- cakk-api/src/main/java/com/cakk/api/service/user/UserService.java
+++ cakk-api/src/main/java/com/cakk/api/service/user/UserService.java
@@ -10,6 +10,9 @@ import com.cakk.api.mapper.UserMapper;
 import com.cakk.domain.mysql.dto.param.user.ProfileUpdateParam;
 import com.cakk.domain.mysql.entity.user.User;
 import com.cakk.domain.mysql.repository.reader.UserReader;
+import com.cakk.domain.mysql.repository.writer.BusinessInformationWriter;
+import com.cakk.domain.mysql.repository.writer.CakeLikeWriter;
+import com.cakk.domain.mysql.repository.writer.CakeShopLikeWriter;
 import com.cakk.domain.mysql.repository.writer.UserWriter;
 
 @Service
@@ -18,6 +21,9 @@ public class UserService {
 
 	private final UserReader userReader;
 	private final UserWriter userWriter;
+	private final CakeShopLikeWriter cakeShopLikeWriter;
+	private final CakeLikeWriter cakeLikeWriter;
+	private final BusinessInformationWriter businessInformationWriter;
 
 	@Transactional
 	public void updateInformation(final User signInUser, final ProfileUpdateRequest dto) {
@@ -25,5 +31,16 @@ public class UserService {
 		final ProfileUpdateParam param = UserMapper.supplyProfileUpdateParamBy(dto);
 
 		user.updateProfile(param);
+	}
+
+	@Transactional
+	public void withdraw(final User signInUser) {
+		final User user = userReader.findByUserId(signInUser.getId());
+
+		cakeLikeWriter.deleteAllByUser(user);
+		cakeShopLikeWriter.deleteAllByUser(user);
+		businessInformationWriter.deleteAllByUser(user);
+
+		userWriter.delete(user);
 	}
 }
