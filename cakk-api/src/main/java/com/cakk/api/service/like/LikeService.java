e5d5c20ac01a892f6ab5203f4c21134009098aa1
Hyunseok Ko <kohyunsuk98@gmail.com>
2024-06-10 18:25:51 +0900

    Feature | #74 | @lcomment | 케이크 좋아요 or 좋아요 취소 비즈니스 구현

--- cakk-api/src/main/java/com/cakk/api/service/like/LikeService.java
+++ cakk-api/src/main/java/com/cakk/api/service/like/LikeService.java
@@ -1,24 +1,37 @@
 package com.cakk.api.service.like;
 
+import static java.util.Objects.*;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import lombok.RequiredArgsConstructor;
 
 import com.cakk.api.dto.request.like.LikeCakeSearchRequest;
 import com.cakk.api.dto.response.like.LikeCakeImageListResponse;
 import com.cakk.api.mapper.CakeMapper;
+import com.cakk.common.enums.RedisKey;
 import com.cakk.domain.mysql.dto.param.like.LikeCakeImageResponseParam;
+import com.cakk.domain.mysql.entity.cake.Cake;
+import com.cakk.domain.mysql.entity.cake.CakeLike;
 import com.cakk.domain.mysql.entity.user.User;
 import com.cakk.domain.mysql.repository.reader.CakeLikeReader;
+import com.cakk.domain.mysql.repository.reader.CakeReader;
+import com.cakk.domain.mysql.repository.writer.CakeLikeWriter;
+import com.cakk.domain.redis.repository.LockRedisRepository;
 
 @RequiredArgsConstructor
 @Service
 public class LikeService {
 
+	private final CakeReader cakeReader;
 	private final CakeLikeReader cakeLikeReader;
+	private final CakeLikeWriter cakeLikeWriter;
 
+	private final LockRedisRepository lockRedisRepository;
+
+	@Transactional(readOnly = true)
 	public LikeCakeImageListResponse findCakeImagesByCursorAndLike(final LikeCakeSearchRequest dto, final User signInUser) {
 		final List<LikeCakeImageResponseParam> cakeImages = cakeLikeReader.searchCakeImagesByCursorAndLike(
 			dto.cakeLikeId(),
@@ -27,5 +40,23 @@ public class LikeService {
 		);
 
 		return CakeMapper.supplyLikeCakeImageListResponseBy(cakeImages);
+	}
+
+	@Transactional
+	public void likeCake(final User user, final Long cakeId) {
+		final Cake cake = cakeReader.findById(cakeId);
+		final CakeLike cakeLike = cakeLikeReader.findOrNullByUserAndCake(user, cake);
+
+		lockRedisRepository.executeWithLock(RedisKey.LOCK_CAKE_LIKE, 1000L,
+			() -> likeCakeOrCancel(cakeLike, user, cake)
+		);
+	}
+
+	private void likeCakeOrCancel(final CakeLike cakeLike, final User user, final Cake cake) {
+		if (isNull(cakeLike)) {
+			cakeLikeWriter.like(cake, user);
+		} else {
+			cakeLikeWriter.cancelLike(cakeLike);
+		}
 	}
 }
