cf147f7d64bb973553f90d898db590c114780b66
김태용 <amico741@naver.com>
2024-06-12 20:53:01 +0900

    Feature | #72 | @YongsHub | CakeShops에서 검색으로 인해 영업일 매칭 컬렉션 기능 추가

--- cakk-domain/mysql/src/main/java/com/cakk/domain/mysql/bo/CakeShops.java
+++ cakk-domain/mysql/src/main/java/com/cakk/domain/mysql/bo/CakeShops.java
@@ -10,7 +10,9 @@ import java.util.Objects;
 import com.cakk.common.enums.ReturnCode;
 import com.cakk.common.exception.CakkException;
 import com.cakk.domain.mysql.dto.param.cake.CakeImageResponseParam;
+import com.cakk.domain.mysql.dto.param.shop.CakeShopByKeywordParam;
 import com.cakk.domain.mysql.dto.param.shop.CakeShopParam;
+import com.cakk.domain.mysql.dto.param.shop.ShopOperationParam;
 import com.cakk.domain.mysql.entity.shop.CakeShop;
 
 public class CakeShops {
@@ -18,13 +20,39 @@ public class CakeShops {
 	private List<CakeShopParam> cakeShops;
 
 	public CakeShops(List<CakeShop> cakeShops, List<CakeImageResponseParam> cakes) {
+		Map<Long, CakeShopParam> map = new HashMap<>();
 		this.cakeShops = new ArrayList<>();
 		initCakeShopByLocationBased(cakeShops);
-		matchRepresentCakeImagesByMaxFour(cakes);
+		matchRepresentCakeImagesByMaxFour(map, cakes);
+	}
+
+	public CakeShops(
+		List<CakeShopByKeywordParam> cakeShops,
+		List<ShopOperationParam> cakeShopOperations,
+		List<CakeImageResponseParam> cakeImages
+		) {
+		Map<Long, CakeShopParam> map = new HashMap<>();
+		this.cakeShops = new ArrayList<>();
+		initCakeShopBySearchKeyword(cakeShops);
+		matchRepresentCakeImagesByMaxFour(map, cakeImages);
+		matchCakeShopOperations(map, cakeShopOperations);
 	}
 
 	public List<CakeShopParam> getCakeShops() {
 		return cakeShops;
+	}
+
+	private void initCakeShopBySearchKeyword(List<CakeShopByKeywordParam> cakeShops) {
+		cakeShops.forEach(cakeShop -> this.cakeShops.add(
+			CakeShopParam.builder()
+				.cakeShopId(cakeShop.cakeShopId())
+				.cakeShopBio(cakeShop.cakeShopBio())
+				.cakeShopName(cakeShop.cakeShopName())
+				.thumbnailUrl(cakeShop.thumbnailUrl())
+				.cakeImageUrls(new HashSet<>())
+				.operationDays(new ArrayList<>())
+				.build()
+		));
 	}
 
 	private void initCakeShopByLocationBased(List<CakeShop> cakeShops) {
@@ -40,9 +68,7 @@ public class CakeShops {
 		));
 	}
 
-	private void matchRepresentCakeImagesByMaxFour(List<CakeImageResponseParam> cakes) {
-		Map<Long, CakeShopParam> map = new HashMap<>();
-
+	private void matchRepresentCakeImagesByMaxFour(Map<Long, CakeShopParam> map, List<CakeImageResponseParam> cakes) {
 		for (CakeImageResponseParam cake : cakes) {
 			CakeShopParam param;
 
@@ -56,6 +82,21 @@ public class CakeShops {
 			if (isCakeImageUrlsCountLessThanFour(param)) {
 				param.addCakeImageUrl(cake.cakeImageUrl());
 			}
+		}
+	}
+
+	private void matchCakeShopOperations(Map<Long, CakeShopParam> map, List<ShopOperationParam> cakeOperations) {
+		for (ShopOperationParam cakeOperation : cakeOperations) {
+			CakeShopParam param;
+
+			if (map.containsKey(cakeOperation.cakeShopId())) {
+				param = map.get(cakeOperation.cakeShopId());
+			} else {
+				param = findCakeShop(cakeOperation.cakeShopId());
+				map.put(cakeOperation.cakeShopId(), param);
+			}
+
+			param.setOperationDay(cakeOperation);
 		}
 	}
 
