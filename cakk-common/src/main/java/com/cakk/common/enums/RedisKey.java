515fecee715b07a19a7d0940d9ca4c3eeb79b45a
Hyunseok Ko <kohyunsuk98@gmail.com>
2024-06-14 15:47:30 +0900

    Feature | #80 | @lcomment | 케이크 조회수 관련 redis repository 구현

--- cakk-common/src/main/java/com/cakk/common/enums/RedisKey.java
+++ cakk-common/src/main/java/com/cakk/common/enums/RedisKey.java
@@ -8,6 +8,7 @@ import lombok.RequiredArgsConstructor;
 public enum RedisKey {
 
 	SEARCH_KEYWORD("SEARCH::keyword"),
+	VIEW_CAKE("VIEW::cake"),
 	LOCK_CAKE_HEART("LOCK::cake-heart"),
 	LOCK_SHOP_HEART("LOCK::shop-heart");
 
--- cakk-domain/redis/src/main/java/com/cakk/domain/redis/template/RedisZSetTemplate.java
+++ cakk-domain/redis/src/main/java/com/cakk/domain/redis/template/RedisZSetTemplate.java
@@ -4,11 +4,13 @@ import java.util.List;
 
 public interface RedisZSetTemplate<T> {
 
-	void save(String key, String value);
+	void save(String key, T value);
 
-	void increaseScore(String key, String value, int delta);
+	void increaseScore(String key, T value, int delta);
 
 	List<T> findAllReverseScore(String key, long count);
+
+	List<T> findAllReverseScore(String key, long offset, long count);
 
 	void removeAll(String key);
 }
