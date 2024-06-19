0ebe42650e47415e57f11f967f12a7c77d2a3577
Hyunseok Ko <kohyunsuk98@gmail.com>
2024-06-19 12:41:27 +0900

    Refactor | #103 | @lcomment | listener 패키지 수정

--- cakk-api/src/main/java/com/cakk/api/listener/shop/CertificationEventListener.java
+++ cakk-api/src/main/java/com/cakk/api/listener/shop/CertificationEventListener.java
@@ -1,4 +1,4 @@
-package com.cakk.api.event.shop.listener;
+package com.cakk.api.event.shop;
 
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
