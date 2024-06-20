64c789430d1e17d144475fcb87a00820721abe88
Hyunseok Ko <kohyunsuk98@gmail.com>
2024-06-20 18:56:05 +0900

    Feature | #107 | @lcomment | batch 관련 utility 구현

---
+++ cakk-batch/src/main/java/com/cakk/batch/utils/BatchConstants.java
@@ -0,0 +1,11 @@
+package com.cakk.batch.utils;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class BatchConstants {
+
+	public static final String CAKE_ID_LIST = "cakeIdList";
+	public static final String KEYWORD_LIST = "keywordList";
+}
