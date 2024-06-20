a2920b6f17cd28865db89d8a671d63796d631c42
Hyunseok Ko <kohyunsuk98@gmail.com>
2024-06-20 18:56:49 +0900

    Feature | #107 | @lcomment | 레디스 초기화 관련 tasklet 구현

---
+++ cakk-batch/src/main/java/com/cakk/batch/tasklet/ClearKeywordListTasklet.java
@@ -0,0 +1,41 @@
+package com.cakk.batch.tasklet;
+
+import static java.util.Objects.*;
+
+import java.util.List;
+
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.item.ExecutionContext;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+import com.cakk.batch.utils.BatchConstants;
+import com.cakk.batch.utils.BatchUtils;
+import com.cakk.domain.redis.repository.KeywordRedisRepository;
+
+@StepScope
+@Component
+@RequiredArgsConstructor
+public class ClearKeywordListTasklet implements Tasklet {
+
+	private final KeywordRedisRepository keywordRedisRepository;
+
+	@Override
+	public RepeatStatus execute(final StepContribution contribution, final ChunkContext chunkContext) {
+		final ExecutionContext executionContext = BatchUtils.getExecutionContext(chunkContext);
+		final List<String> keywords = (List<String>) executionContext.get(BatchConstants.KEYWORD_LIST);
+
+		if (nonNull(keywords) && !keywords.isEmpty()) {
+			keywords.forEach(keywordRedisRepository::deleteByValue);
+		}
+
+		contribution.setExitStatus(ExitStatus.COMPLETED);
+		return RepeatStatus.FINISHED;
+	}
+}
