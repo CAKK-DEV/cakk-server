7375c6374f429a59565e7e8367af50be57f1ef6d
김태용 <amico741@naver.com>
2024-06-21 23:40:28 +0900

    Fix | #102 | @YongsHub | 검색 시, 위치 정보 제공 미동의 케이스를 위한 검증 제거

--- cakk-api/src/main/java/com/cakk/api/dto/request/cake/CakeSearchByLocationRequest.java
+++ cakk-api/src/main/java/com/cakk/api/dto/request/cake/CakeSearchByLocationRequest.java
@@ -2,7 +2,6 @@ package com.cakk.api.dto.request.cake;
 
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotNull;
 
 import com.cakk.api.mapper.PointMapper;
 import com.cakk.domain.mysql.dto.param.cake.CakeSearchParam;
@@ -10,9 +9,9 @@ import com.cakk.domain.mysql.dto.param.cake.CakeSearchParam;
 public record CakeSearchByLocationRequest(
 	Long cakeId,
 	String keyword,
-	@NotNull @Min(-90) @Max(90)
+	@Min(-90) @Max(90)
 	Double latitude,
-	@NotNull @Min(-180) @Max(180)
+	@Min(-180) @Max(180)
 	Double longitude,
 	Integer pageSize
 ) {
