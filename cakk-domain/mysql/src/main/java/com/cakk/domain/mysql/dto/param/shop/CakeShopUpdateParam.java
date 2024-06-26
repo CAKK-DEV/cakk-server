283e3ad9dd6a56a86ad194033d66337af44c0d72
김태용 <amico741@naver.com>
2024-06-26 14:22:21 +0900

    Refactor | #121 | @YongsHub | 케이크샵 기본정보 수정에서 이미지 url 필드 추가

--- cakk-domain/mysql/src/main/java/com/cakk/domain/mysql/dto/param/shop/CakeShopUpdateParam.java
+++ cakk-domain/mysql/src/main/java/com/cakk/domain/mysql/dto/param/shop/CakeShopUpdateParam.java
@@ -3,6 +3,7 @@ package com.cakk.domain.mysql.dto.param.shop;
 import com.cakk.domain.mysql.entity.user.User;
 
 public record CakeShopUpdateParam(
+	String thumbnailUrl,
 	String shopName,
 	String shopBio,
 	String shopDescription,
