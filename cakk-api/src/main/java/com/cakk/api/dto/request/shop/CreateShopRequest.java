700d0d1e4dc93e934472b32b37057d3752209994
김태용 <amico741@naver.com>
2024-06-26 14:18:32 +0900

    Refactor | #121 | @YongsHub | 생성 dto validation 추가

--- cakk-api/src/main/java/com/cakk/api/dto/request/shop/CreateShopRequest.java
+++ cakk-api/src/main/java/com/cakk/api/dto/request/shop/CreateShopRequest.java
@@ -1,21 +1,27 @@
 package com.cakk.api.dto.request.shop;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 import com.cakk.api.annotation.OperationDay;
 
 public record CreateShopRequest(
+	@Size(max = 20)
 	String businessNumber,
-	@OperationDay
+	@NotNull @OperationDay
 	OperationDays operationDays,
-	@NotBlank
+	@NotBlank @Size(max = 30)
 	String shopName,
+	@Size(max = 40)
 	String shopBio,
+	@Size(max = 500)
 	String shopDescription,
-	@NotNull
+	@NotNull @Min(-90) @Max(90)
 	Double latitude,
-	@NotNull
+	@NotNull @Min(-180) @Max(180)
 	Double longitude
 ) {
 }
