26a0efdfb0edf1a17fd8455e62756d2ca9895eb8
Hyunseok Ko <kohyunsuk98@gmail.com>
2024-07-09 18:29:29 +0900

    Test | #157 | @lcomment | 유닛 테스트 추가 작성

--- cakk-api/src/test/java/com/cakk/api/ArchitectureTest.java
+++ cakk-api/src/test/java/com/cakk/api/ArchitectureTest.java
@@ -5,6 +5,7 @@ import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 import com.cakk.api.common.annotation.TestWithDisplayName;
 import com.tngtech.archunit.core.domain.JavaClasses;
@@ -24,25 +25,81 @@ class ArchitectureTest {
 			.importPackages("com.cakk");
 	}
 
+	@TestWithDisplayName("request 패키지 안에 있는 클래스들은 Request로 끝나야 한다.")
+	void request() {
+		ArchRule rule = classes()
+			.that().resideInAPackage("..request..")
+			.and().areTopLevelClasses()
+			.should().haveSimpleNameEndingWith("Request");
+
+		rule.check(javaClasses);
+	}
+
 	@TestWithDisplayName("response 패키지 안에 있는 클래스들은 Response로 끝나야 한다.")
 	void response() {
 		ArchRule rule = classes()
-			.that().resideInAPackage("..response")
+			.that().resideInAPackage("..response..")
 			.and().areTopLevelClasses()
 			.should().haveSimpleNameEndingWith("Response");
 
 		rule.check(javaClasses);
 	}
 
-	@Disabled
-	@TestWithDisplayName("Provider 클래스는 Service, Filter, Provider 클래스에만 의존해야 한다.")
-	void providerDependency() {
+	@TestWithDisplayName("param 패키지 안에 있는 클래스들은 Param으로 끝나야 한다.")
+	void param() {
 		ArchRule rule = classes()
-			.that().haveNameMatching(".*Provider")
-			.and().areNotEnums()
-			.should().onlyHaveDependentClassesThat().haveSimpleNameEndingWith("Service")
-			.orShould().onlyHaveDependentClassesThat().haveSimpleNameEndingWith("Filter")
-			.orShould().onlyHaveDependentClassesThat().haveSimpleNameEndingWith("Provider");
+			.that().resideInAPackage("..param..")
+			.and().areTopLevelClasses()
+			.should().haveSimpleNameEndingWith("Param");
+
+		rule.check(javaClasses);
+	}
+
+	@TestWithDisplayName("event 패키지 안에 있는 클래스들은 Event로 끝나야 한다.")
+	void event() {
+		ArchRule rule = classes()
+			.that().resideInAPackage("..event..")
+			.and().areTopLevelClasses()
+			.should().haveSimpleNameEndingWith("Event");
+
+		rule.check(javaClasses);
+	}
+
+	@TestWithDisplayName("mapper 패키지 안에 있는 클래스들은 Mapper로 끝나야 한다.")
+	void mapper() {
+		ArchRule rule = classes()
+			.that().resideInAPackage("..mapper..")
+			.and().areTopLevelClasses()
+			.should().haveSimpleNameEndingWith("Mapper");
+
+		rule.check(javaClasses);
+	}
+
+	@TestWithDisplayName("Provider 클래스는 Controller 클래스에 의존하지 않아야 한다.")
+	void providerNotController() {
+		ArchRule rule = noClasses()
+			.that().resideInAnyPackage("..controller..")
+			.should().onlyDependOnClassesThat().resideInAnyPackage("..provider..");
+
+		rule.check(javaClasses);
+	}
+
+	@TestWithDisplayName("Factory 클래스는 Service 클래스에만 의존해야 한다.")
+	void factoryDependency() {
+		ArchRule rule = classes()
+			.that().haveNameMatching(".*Factory")
+			.should().onlyHaveDependentClassesThat().haveSimpleNameEndingWith("Service");
+
+		rule.check(javaClasses);
+	}
+
+	@DisplayName("Controller 는 Service, Repository 에 의존하지 않아야 한다.")
+	@Test
+	void serviceNotController() {
+		ArchRule rule = noClasses()
+			.that().resideInAnyPackage("..service..", "..repository..")
+			.should().dependOnClassesThat()
+			.resideInAnyPackage("..controller..");
 
 		rule.check(javaClasses);
 	}
---
+++ cakk-api/src/test/java/com/cakk/api/common/base/EventListenerTest.java
@@ -0,0 +1,37 @@
+package com.cakk.api.common.base;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.navercorp.fixturemonkey.FixtureMonkey;
+import com.navercorp.fixturemonkey.api.introspector.BuilderArbitraryIntrospector;
+import com.navercorp.fixturemonkey.api.introspector.ConstructorPropertiesArbitraryIntrospector;
+import com.navercorp.fixturemonkey.api.introspector.FieldReflectionArbitraryIntrospector;
+import com.navercorp.fixturemonkey.jakarta.validation.plugin.JakartaValidationPlugin;
+
+@ActiveProfiles("test")
+@ExtendWith(MockitoExtension.class)
+public abstract class EventListenerTest {
+
+	protected final FixtureMonkey getConstructorMonkey() {
+		return FixtureMonkey.builder()
+			.plugin(new JakartaValidationPlugin())
+			.objectIntrospector(ConstructorPropertiesArbitraryIntrospector.INSTANCE)
+			.build();
+	}
+
+	protected final FixtureMonkey getReflectionMonkey() {
+		return FixtureMonkey.builder()
+			.plugin(new JakartaValidationPlugin())
+			.objectIntrospector(FieldReflectionArbitraryIntrospector.INSTANCE)
+			.build();
+	}
+
+	protected final FixtureMonkey getBuilderMonkey() {
+		return FixtureMonkey.builder()
+			.plugin(new JakartaValidationPlugin())
+			.objectIntrospector(BuilderArbitraryIntrospector.INSTANCE)
+			.build();
+	}
+}
