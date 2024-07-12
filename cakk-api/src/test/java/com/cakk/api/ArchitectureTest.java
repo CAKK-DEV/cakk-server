67ba246fcd201530be49c9f827e37ce099384810
Hyunseok Ko <kohyunsuk98@gmail.com>
2024-07-12 22:22:32 +0900

    Test | #157 | @lcomment | 유닛 테스트 추가 작성

--- cakk-api/src/test/java/com/cakk/api/ArchitectureTest.java
+++ cakk-api/src/test/java/com/cakk/api/ArchitectureTest.java
@@ -75,6 +75,97 @@ class ArchitectureTest {
 		rule.check(javaClasses);
 	}
 
+	@TestWithDisplayName("config 패키지 안에 있는 클래스들은 Config로 끝나야 한다.")
+	void config() {
+		ArchRule rule = classes()
+			.that().resideInAPackage("..config..")
+			.and().areTopLevelClasses()
+			.should().haveSimpleNameEndingWith("Config");
+
+		rule.check(javaClasses);
+	}
+
+	@TestWithDisplayName("factory 패키지 안에 있는 클래스들은 Factory 끝나야 한다.")
+	void factory() {
+		ArchRule rule = classes()
+			.that().resideInAPackage("..factory..")
+			.and().areTopLevelClasses()
+			.should().haveSimpleNameEndingWith("Factory");
+
+		rule.check(javaClasses);
+	}
+
+	@TestWithDisplayName("filter 패키지 안에 있는 클래스들은 Filter 끝나야 한다.")
+	void filter() {
+		ArchRule rule = classes()
+			.that().resideInAPackage("..filter..")
+			.and().areTopLevelClasses()
+			.should().haveSimpleNameEndingWith("Filter");
+
+		rule.check(javaClasses);
+	}
+
+	@TestWithDisplayName("listener 패키지 안에 있는 클래스들은 Listener 끝나야 한다.")
+	void listener() {
+		ArchRule rule = classes()
+			.that().resideInAPackage("..listener..")
+			.and().areTopLevelClasses()
+			.should().haveSimpleNameEndingWith("Listener");
+
+		rule.check(javaClasses);
+	}
+
+	@TestWithDisplayName("provider 패키지 안에 있는 클래스들은 Provider 끝나야 한다.")
+	void provider() {
+		ArchRule rule = classes()
+			.that().resideInAPackage("..provider..")
+			.and().areTopLevelClasses()
+			.should().haveSimpleNameEndingWith("Provider");
+
+		rule.check(javaClasses);
+	}
+
+	@TestWithDisplayName("resolver 패키지 안에 있는 클래스들은 Resolver 끝나야 한다.")
+	void resolver() {
+		ArchRule rule = classes()
+			.that().resideInAPackage("..resolver..")
+			.and().areTopLevelClasses()
+			.should().haveSimpleNameEndingWith("Resolver");
+
+		rule.check(javaClasses);
+	}
+
+	@TestWithDisplayName("validator 패키지 안에 있는 클래스들은 Validator 끝나야 한다.")
+	void validator() {
+		ArchRule rule = classes()
+			.that().resideInAPackage("..validator..")
+			.and().areTopLevelClasses()
+			.should().haveSimpleNameEndingWith("Validator");
+
+		rule.check(javaClasses);
+	}
+
+	@TestWithDisplayName("service 패키지 안에 있는 클래스들은 Service로 끝나야 한다.")
+	void service() {
+		ArchRule rule = classes()
+			.that().resideInAPackage("..service..")
+			.and().areTopLevelClasses()
+			.should().haveSimpleNameEndingWith("Service");
+
+		rule.check(javaClasses);
+	}
+
+	@TestWithDisplayName("controller 패키지 안에 있는 클래스들은 Controller로 끝나야 한다.")
+	void controller() {
+		ArchRule rule = classes()
+			.that().resideInAPackage("..controller..")
+			.and().areTopLevelClasses()
+			.should().haveSimpleNameEndingWith("Controller")
+			.orShould().haveSimpleNameEndingWith("ControllerAdvice");
+
+		rule.check(javaClasses);
+	}
+
 	@TestWithDisplayName("Provider 클래스는 Controller 클래스에 의존하지 않아야 한다.")
 	void providerNotController() {
 		ArchRule rule = noClasses()
