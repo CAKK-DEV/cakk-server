e19d286d9f4b119f4fa0998651ebb4fbf234097d
lcomment <kohyunsuk98@gmail.com>
2024-08-12 14:23:03 +0900

    Refactor | CAKK-28 | kotlin dsl로 리팩토링

---
+++ settings.gradle.kts
@@ -0,0 +1,34 @@
+rootProject.name = "cakk"
+
+include(
+	"cakk-api",
+	"cakk-admin",
+	"cakk-batch",
+	"cakk-client",
+	"cakk-domain:mysql",
+	"cakk-domain:redis",
+	"cakk-external",
+	"cakk-common"
+)
+
+pluginManagement {
+	val kotlinVersion: String by settings
+	val springBootVersion: String by settings
+	val springDependencyManagementVersion: String by settings
+	val ktlintVersion: String by settings
+
+	resolutionStrategy {
+		eachPlugin {
+			when (requested.id.id) {
+				"org.jetbrains.kotlin.jvm" -> useVersion(kotlinVersion)
+				"org.jetbrains.kotlin.kapt" -> useVersion(kotlinVersion)
+				"org.jetbrains.kotlin.plugin.jpa" -> useVersion(kotlinVersion)
+				"org.jetbrains.kotlin.plugin.spring" -> useVersion(kotlinVersion)
+				"org.springframework.boot" -> useVersion(springBootVersion)
+				"io.spring.dependency-management" -> useVersion(springDependencyManagementVersion)
+				"org.jlleitschuh.gradle.ktlint" -> useVersion(ktlintVersion)
+				"org.jlleitschuh.gradle.ktlint-idea" -> useVersion(ktlintVersion)
+			}
+		}
+	}
+}
