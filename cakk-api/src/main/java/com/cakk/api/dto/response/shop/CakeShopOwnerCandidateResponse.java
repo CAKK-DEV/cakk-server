76b4580134f1059bbfc6d85e3d574db9ecf4380a
김태용 <amico741@naver.com>
2024-08-14 11:26:12 +0900

    Fix | CAKK-26 | dto 네이밍 수정

--- cakk-api/src/main/java/com/cakk/api/dto/response/shop/CakeShopOwnerCandidateResponse.java
+++ cakk-api/src/main/java/com/cakk/api/dto/response/shop/CakeShopOwnerCandidateResponse.java
@@ -1,11 +1,11 @@
 package com.cakk.api.dto.response.shop;
 
-import java.util.List;
-
-import com.cakk.api.dto.param.operation.OwnerCandidateParam;
-
 public record CakeShopOwnerCandidateResponse(
-	List<OwnerCandidateParam> candidates
+	Long userId,
+	Long cakeShopId,
+	String email,
+	String businessRegistrationImageUrl,
+	String idCardImageUrl,
+	String emergencyContact
 ) {
 }
-
