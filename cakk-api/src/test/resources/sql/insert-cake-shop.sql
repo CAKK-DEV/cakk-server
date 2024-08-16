353c6050d38773eb70eadca98f18dc100c777f3d
김태용 <amico741@naver.com>
2024-08-16 19:24:31 +0900

    Fix | CAKK-36 | sql file 테이블 구조 변경으로 수정

--- cakk-api/src/test/resources/sql/insert-cake-shop.sql
+++ cakk-api/src/test/resources/sql/insert-cake-shop.sql
@@ -9,19 +9,18 @@ SET @g8 = 'Point(37.541530 127.054164)';
 SET @g9 = 'Point(37.543343 127.052609)';
 SET @g10 = 'Point(37.541530 127.054164)';
 
-insert into cake_shop (shop_id, thumbnail_url, shop_name, shop_address, shop_bio, shop_description, location, like_count, heart_count, linked_flag,
-                       created_at, updated_at)
-values (1, 'thumbnail_url1', '케이크 맛집1', '케이크 맛집입니다.', '서울시 강남구 어쩌고로1', '케이크 맛집입니다.', ST_GeomFromText(@g1, 4326), 0, 0, false, now(), now()),
-       (2, 'thumbnail_url2', '케이크 맛집2', '케이크 맛집입니다.', '서울시 강남구 어쩌고로2', '케이크 맛집입니다.', ST_GeomFromText(@g2, 4326), 0, 0, false, now(), now()),
-       (3, 'thumbnail_url3', '케이크 맛집3', '케이크 맛집입니다.', '서울시 강남구 어쩌고로3', '케이크 맛집입니다.', ST_GeomFromText(@g3, 4326), 0, 0, false, now(), now()),
-       (4, 'thumbnail_url4', '케이크 맛집4', '케이크 맛집입니다.', '서울시 강남구 어쩌고로3', '케이크 맛집입니다.', ST_GeomFromText(@g4, 4326), 0, 0, false, now(), now()),
-       (5, 'thumbnail_url5', '케이크 맛집5', '케이크 맛집입니다.', '서울시 강남구 어쩌고로3', '케이크 맛집입니다.', ST_GeomFromText(@g5, 4326), 0, 0, false, now(), now()),
-       (6, 'thumbnail_url6', '케이크 맛집6', '케이크 맛집입니다.', '서울시 강남구 어쩌고로3', '케이크 맛집입니다.', ST_GeomFromText(@g6, 4326), 0, 0, false, now(), now()),
-       (7, 'thumbnail_url7', '케이크 맛집7', '케이크 맛집입니다.', '서울시 강남구 어쩌고로3', '케이크 맛집입니다.', ST_GeomFromText(@g7, 4326), 0, 0, false, now(), now()),
-       (8, 'thumbnail_url8', '케이크 맛집8', '케이크 맛집입니다.', '서울시 강남구 어쩌고로3', '케이크 맛집입니다.', ST_GeomFromText(@g8, 4326), 0, 0, false, now(), now()),
-       (9, 'thumbnail_url9', '케이크 맛집9', '케이크 맛집입니다.', '서울시 강남구 어쩌고로3', '케이크 맛집입니다.', ST_GeomFromText(@g9, 4326), 0, 0, false, now(), now()),
-       (10, 'thumbnail_url10', '케이크 맛집10', '케이크 맛집입니다.', '서울시 강남구 어쩌고로3', '케이크 맛집입니다.', ST_GeomFromText(@g10, 4326), 0, 0, false, now(), now()),
-       (11, 'thumbnail_url10', '케이크 맛집11', '케이크 맛집입니다.', '서울시 강남구 어쩌고로3', '케이크 맛집입니다.', ST_GeomFromText(@g10, 4326), 0, 0, false, now(), now());
+insert into cake_shop (shop_id, thumbnail_url, shop_name, shop_address, shop_bio, shop_description, location, like_count, heart_count, created_at, updated_at)
+values (1, 'thumbnail_url1', '케이크 맛집1', '케이크 맛집입니다.', '서울시 강남구 어쩌고로1', '케이크 맛집입니다.', ST_GeomFromText(@g1, 4326), 0, 0, now(), now()),
+       (2, 'thumbnail_url2', '케이크 맛집2', '케이크 맛집입니다.', '서울시 강남구 어쩌고로2', '케이크 맛집입니다.', ST_GeomFromText(@g2, 4326), 0, 0, now(), now()),
+       (3, 'thumbnail_url3', '케이크 맛집3', '케이크 맛집입니다.', '서울시 강남구 어쩌고로3', '케이크 맛집입니다.', ST_GeomFromText(@g3, 4326), 0, 0, now(), now()),
+       (4, 'thumbnail_url4', '케이크 맛집4', '케이크 맛집입니다.', '서울시 강남구 어쩌고로3', '케이크 맛집입니다.', ST_GeomFromText(@g4, 4326), 0, 0, now(), now()),
+       (5, 'thumbnail_url5', '케이크 맛집5', '케이크 맛집입니다.', '서울시 강남구 어쩌고로3', '케이크 맛집입니다.', ST_GeomFromText(@g5, 4326), 0, 0, now(), now()),
+       (6, 'thumbnail_url6', '케이크 맛집6', '케이크 맛집입니다.', '서울시 강남구 어쩌고로3', '케이크 맛집입니다.', ST_GeomFromText(@g6, 4326), 0, 0, now(), now()),
+       (7, 'thumbnail_url7', '케이크 맛집7', '케이크 맛집입니다.', '서울시 강남구 어쩌고로3', '케이크 맛집입니다.', ST_GeomFromText(@g7, 4326), 0, 0, now(), now()),
+       (8, 'thumbnail_url8', '케이크 맛집8', '케이크 맛집입니다.', '서울시 강남구 어쩌고로3', '케이크 맛집입니다.', ST_GeomFromText(@g8, 4326), 0, 0, now(), now()),
+       (9, 'thumbnail_url9', '케이크 맛집9', '케이크 맛집입니다.', '서울시 강남구 어쩌고로3', '케이크 맛집입니다.', ST_GeomFromText(@g9, 4326), 0, 0, now(), now()),
+       (10, 'thumbnail_url10', '케이크 맛집10', '케이크 맛집입니다.', '서울시 강남구 어쩌고로3', '케이크 맛집입니다.', ST_GeomFromText(@g10, 4326), 0, 0, now(), now()),
+       (11, 'thumbnail_url10', '케이크 맛집11', '케이크 맛집입니다.', '서울시 강남구 어쩌고로3', '케이크 맛집입니다.', ST_GeomFromText(@g10, 4326), 0, 0, now(), now());
 
 insert into cake (cake_id, shop_id, cake_image_url, heart_count, created_at, updated_at)
 values (1, 1, 'cake_image_url1', 0, now(), now()),
@@ -86,15 +85,17 @@ values (1, 1, 'web', 'https://www.cake-shop1.com', now(), now()),
        (6, 3, 'web', 'https://www.cake-shop3.com', now(), now()),
        (7, 3, 'kakaotalk', 'https://www.kakaotalk.com/cake-shop3', now(), now());
 
-insert into business_information(business_number, shop_id, user_id)
-values ('010-3375-5555', 1, 1),
-       ('010-3375-5555', 2, 2),
-       ('010-3375-5555', 3, 3),
-       ('010-3375-5555', 4, 4),
-       ('010-3375-5555', 5, 5),
-       ('010-3375-5555', 6, 6),
-       ('010-3375-5555', 7, 7),
-       ('010-3375-5555', 8, 8),
-       ('010-3375-5555', 9, 9),
-       ('010-3375-5555', 10, 10),
-       ('010-3375-5555', 11, null);
+insert into business_information(business_number, business_registration_image_url, id_card_image_url, emergency_contact, shop_id, user_id)
+values ('010-3375-5555', NULL, NULL, NULL, 2, 2),
+       ('010-3375-5555', NULL, NULL, NULL, 3, 3),
+       ('010-3375-5555', NULL, NULL, NULL, 4, 4),
+       ('010-3375-5555', NULL, NULL, NULL, 5, 5),
+       ('010-3375-5555', NULL, NULL, NULL, 6, 6),
+       ('010-3375-5555', NULL, NULL, NULL, 7, 7),
+       ('010-3375-5555', NULL, NULL, NULL, 8, 8),
+       ('010-3375-5555', NULL, NULL, NULL, 9, 9),
+       ('010-3375-5555', NULL, NULL, NULL, 10, 10),
+       ('010-3375-5555', NULL, NULL, NULL, 11, null);
+
+insert into business_information(business_number, business_registration_image_url, id_card_image_url, emergency_contact, shop_id, user_id, verification_status)
+values ('010-3375-5555', NULL, NULL, NULL, 1, 1, 1);
