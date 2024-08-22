f3c6a6dabdb39bf64d27c26f0780325faad332d0
lcomment <kohyunsuk98@gmail.com>
2024-08-22 14:27:46 +0900

    Refactor | CAKK-41 | Param에 Builder 추

--- cakk-domain/mysql/src/main/java/com/cakk/domain/mysql/dto/param/operation/UpdateShopOperationParam.java
+++ cakk-domain/mysql/src/main/java/com/cakk/domain/mysql/dto/param/operation/UpdateShopOperationParam.java
@@ -2,9 +2,12 @@ package com.cakk.domain.mysql.dto.param.operation;
 
 import java.util.List;
 
+import lombok.Builder;
+
 import com.cakk.domain.mysql.entity.shop.CakeShopOperation;
 import com.cakk.domain.mysql.entity.user.User;
 
+@Builder
 public record UpdateShopOperationParam(
 	List<CakeShopOperation> cakeShopOperations,
 	User user,
--- cakk-domain/mysql/src/main/java/com/cakk/domain/mysql/dto/param/shop/CakeShopUpdateParam.java
+++ cakk-domain/mysql/src/main/java/com/cakk/domain/mysql/dto/param/shop/CakeShopUpdateParam.java
@@ -1,7 +1,10 @@
 package com.cakk.domain.mysql.dto.param.shop;
 
+import lombok.Builder;
+
 import com.cakk.domain.mysql.entity.user.User;
 
+@Builder
 public record CakeShopUpdateParam(
 	String thumbnailUrl,
 	String shopName,
