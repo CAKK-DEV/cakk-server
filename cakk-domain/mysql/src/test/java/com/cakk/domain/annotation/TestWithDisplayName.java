3919bf3d8c47300272324c90fb9741fe7ef7e337
lcomment <kohyunsuk98@gmail.com>
2024-08-28 22:30:07 +0900

    Test | CAKK-58 | Facade 테스트 추상 클래스 구축 및 테스트 작성

---
+++ cakk-domain/mysql/src/test/java/com/cakk/domain/annotation/TestWithDisplayName.java
@@ -0,0 +1,41 @@
+package com.cakk.domain.annotation;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
+
+import org.junit.jupiter.api.DisplayNameGeneration;
+import org.junit.jupiter.api.DisplayNameGenerator;
+import org.junit.jupiter.api.Test;
+
+@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Test
+@DisplayNameGeneration(TestWithDisplayName.TestDisplayNameGenerator.class)
+public @interface TestWithDisplayName {
+
+	String value() default "";
+
+	class TestDisplayNameGenerator extends DisplayNameGenerator.Standard {
+
+		@Override
+		public String generateDisplayNameForClass(Class<?> testClass) {
+			final TestWithDisplayName testWithDisplayName = testClass.getAnnotation(TestWithDisplayName.class);
+
+			if (testWithDisplayName != null && !testWithDisplayName.value().isEmpty()) {
+				return testWithDisplayName.value();
+			}
+
+			return super.generateDisplayNameForClass(testClass);
+		}
+
+		@Override
+		public String generateDisplayNameForMethod(Class<?> testClass, Method testMethod) {
+			return testMethod.getName();
+		}
+	}
+}
