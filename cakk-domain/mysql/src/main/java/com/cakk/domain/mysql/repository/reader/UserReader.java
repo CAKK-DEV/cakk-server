ad486de0c1ce94496b6fb70cd197b07d19726d84
lcomment <kohyunsuk98@gmail.com>
2024-08-28 22:29:47 +0900

    Refactor | CAKK-58 | Service 레이어에 Facade 패턴 적용

--- cakk-domain/mysql/src/main/java/com/cakk/domain/mysql/repository/reader/UserReader.java
+++ cakk-domain/mysql/src/main/java/com/cakk/domain/mysql/repository/reader/UserReader.java
@@ -1,5 +1,7 @@
 package com.cakk.domain.mysql.repository.reader;
 
+import static java.util.Objects.*;
+
 import java.util.List;
 
 import lombok.RequiredArgsConstructor;
@@ -9,12 +11,14 @@ import com.cakk.common.exception.CakkException;
 import com.cakk.domain.mysql.annotation.Reader;
 import com.cakk.domain.mysql.entity.user.User;
 import com.cakk.domain.mysql.repository.jpa.UserJpaRepository;
+import com.cakk.domain.mysql.repository.query.UserQueryRepository;
 
 @Reader
 @RequiredArgsConstructor
 public class UserReader {
 
 	private final UserJpaRepository userJpaRepository;
+	private final UserQueryRepository userQueryRepository;
 
 	public User findByUserId(final Long userId) {
 		return userJpaRepository.findById(userId).orElseThrow(() -> new CakkException(ReturnCode.NOT_EXIST_USER));
@@ -22,6 +26,16 @@ public class UserReader {
 
 	public User findByProviderId(final String providerId) {
 		return userJpaRepository.findByProviderId(providerId).orElseThrow(() -> new CakkException(ReturnCode.NOT_EXIST_USER));
+	}
+
+	public User findByIdWithAll(final Long userId) {
+		final User user = userQueryRepository.searchByIdWithAll(userId);
+
+		if (isNull(user)) {
+			throw new CakkException(ReturnCode.NOT_EXIST_USER);
+		}
+
+		return user;
 	}
 
 	public List<User> findAll() {
