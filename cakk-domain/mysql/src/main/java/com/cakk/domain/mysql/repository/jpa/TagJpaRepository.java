253755d4af71c3ab4961f889588db788865ef9d4
김태용 <amico741@naver.com>
2024-08-28 17:53:08 +0900

    Refactor | CAKK-57 | 태그 이름으로 태그 가져오는 로직 리팩토링

--- cakk-domain/mysql/src/main/java/com/cakk/domain/mysql/repository/jpa/TagJpaRepository.java
+++ cakk-domain/mysql/src/main/java/com/cakk/domain/mysql/repository/jpa/TagJpaRepository.java
@@ -1,5 +1,6 @@
 package com.cakk.domain.mysql.repository.jpa;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,4 +12,6 @@ import com.cakk.domain.mysql.entity.cake.Tag;
 public interface TagJpaRepository extends JpaRepository<Tag, Long> {
 
 	Optional<Tag> findTagByTagName(String tagName);
+
+	List<Tag> findTagsByTagNameIsIn(List<String> tagNames);
 }
