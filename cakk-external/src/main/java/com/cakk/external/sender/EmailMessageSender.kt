c42acc1098315c37ef798bd10f4bfb07b828c376
lcomment <kohyunsuk98@gmail.com>
2024-08-31 00:44:11 +0900

    Refactor | CAKK-61 | Dip 위반 문제 해결

--- cakk-external/src/main/java/com/cakk/external/sender/EmailMessageSender.kt
+++ cakk-external/src/main/java/com/cakk/external/sender/EmailMessageSender.kt
@@ -3,12 +3,10 @@ package com.cakk.external.sender
 import jakarta.mail.internet.MimeMessage
 
 import org.springframework.mail.javamail.JavaMailSender
-import org.springframework.stereotype.Component
 
 import com.cakk.common.enums.ReturnCode
 import com.cakk.common.exception.CakkException
 
-@Component
 class EmailMessageSender(
 	private val mailSender: JavaMailSender
 ) : MessageSender<MimeMessage> {
