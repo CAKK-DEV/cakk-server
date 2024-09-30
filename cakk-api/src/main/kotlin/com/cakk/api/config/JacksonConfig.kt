0d91c5daae0c072bcc583840423b6eb9e8e86acd
Hyunseok Ko <56003992+lcomment@users.noreply.github.com>
2024-09-30 14:23:18 +0900

    Hotfix | CAKK-89 | objectmapper 설정 수정

--- cakk-api/src/main/kotlin/com/cakk/api/config/JacksonConfig.kt
+++ cakk-api/src/main/kotlin/com/cakk/api/config/JacksonConfig.kt
@@ -1,8 +1,10 @@
-package com.cakk.domain.mysql.config
+package com.cakk.api.config
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinFeature
+import com.fasterxml.jackson.module.kotlin.KotlinModule
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
@@ -13,6 +15,16 @@ class JacksonConfig {
         val objectMapper = ObjectMapper()
         objectMapper.registerModule(JavaTimeModule())
         objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+		objectMapper.registerModule(
+			KotlinModule.Builder()
+			.withReflectionCacheSize(512)
+			.configure(KotlinFeature.NullToEmptyCollection, false)
+			.configure(KotlinFeature.NullToEmptyMap, false)
+			.configure(KotlinFeature.NullIsSameAsDefault, false)
+			.configure(KotlinFeature.SingletonSupport, false)
+			.configure(KotlinFeature.StrictNullChecks, false)
+			.build())
+
         return objectMapper
     }
 }
