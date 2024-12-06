fbe0cf529fc2f5980f8d63eca3eab0f128484e57
lcomment <kohyunsuk98@gmail.com>
2024-12-06 17:24:45 +0900

    Test | CAKK-95 | Fixture 수정

--- cakk-api/src/test/java/com/cakk/api/provider/AppleAuthProviderTest.kt
+++ cakk-api/src/test/java/com/cakk/api/provider/AppleAuthProviderTest.kt
@@ -49,7 +49,7 @@ class AppleAuthProviderTest : MockitoTest() {
 		val oidcPublicKeyList = fixtureMonkey.giveMeBuilder(OidcPublicKeyList::class.java)
 			.set("keys", Arbitraries.of(oidcPublicKeyFixture).list().ofMinSize(1).ofMaxSize(10))
 			.sample()
-		val publicKey = fixtureMonkey.giveMeOne(PublicKey::class.java)
+		val publicKey = mock(PublicKey::class.java)
 		val claims = fixtureMonkey.giveMeBuilder(DefaultClaims::class.java).sample()
 		claims.setSubject(Arbitraries.strings().withCharRange('a', 'z').ofMinLength(5).ofMaxLength(10).sample())
 
